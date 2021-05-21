eecb5e200a0cb4a9b41d362d00363800e335b148
eknag <eknag@andrew.cmu.edu>
2021-05-20 21:20:17 -0400

    modified timeout to 3000ms

--- mobile/mobile.js
+++ mobile/mobile.js
@@ -52,7 +52,7 @@ function buttonClicked() {
     if (scoreResetter) {
         clearTimeout(scoreResetter);
     }
-    scoreResetter = window.setTimeout(resetGame, 750);
+    scoreResetter = window.setTimeout(resetGame, 3000);
 }
 
 document.getElementById('circle').onclick = buttonClicked;
